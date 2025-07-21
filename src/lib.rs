92050a19fb8b9a50f547b7b7b9fc6673df53ef60
yhw2003 <haowu.yang@stepai.cn>
2025-07-21 11:48:49 +0800

    笑死我了

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::ItemFn;
+use syn::parse_macro_input;
+
+#[proc_macro_attribute]
+pub fn thatsafe(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemFn);
+    let vis = input.vis;
+    let sig = input.sig;
+    let block = input.block;
+    let mut new_sig = sig.clone();
+    new_sig.unsafety = None;
+    quote! {
+        #vis #new_sig {
+            unsafe {
+                #block
+            }
+        }
+    }
+    .into()
+}
